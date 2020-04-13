e99fa5c344f54b034b42c3307f9ad35fc307f438
unknown <usamnet000@gmail.com>
2020-04-13 10:28:44 -0700

    Updates

---
+++ precache-manifest.f03f9b30c74bc6672780251073b8fbfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98125f9914a83bf4a6a03df6c84a2a0",
+    "url": "/PortfolioInReact/index.html"
+  },
+  {
+    "revision": "46e2b9ef5bf6583c3c2e",
+    "url": "/PortfolioInReact/static/css/main.fc6715d9.chunk.css"
+  },
+  {
+    "revision": "8a00872318e7e0cb0cfc",
+    "url": "/PortfolioInReact/static/js/2.45f82b93.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/PortfolioInReact/static/js/2.45f82b93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "46e2b9ef5bf6583c3c2e",
+    "url": "/PortfolioInReact/static/js/main.d9d4c81c.chunk.js"
+  },
+  {
+    "revision": "563e4c79961c3b80e2b5",
+    "url": "/PortfolioInReact/static/js/runtime-main.8eeacf67.js"
+  }
+]);
